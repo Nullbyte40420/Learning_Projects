d890c64698cf2381f75f53cc2e4f2069afb729ad
Abhilash5542 <abhilashsingh85173@gmail.com>
2025-10-27 18:38:44 +0530

    Choice_V2

--- Choice_game/choice.py
+++ Choice_game/choice.py
@@ -1,22 +1,17 @@
-print("🕵️.The Lost Treasure")
-print("You are an explorer deep in an ancient jungle.\n"
-"Legends say a proceless treasure lies hidden somewhere\n" 
-"within. After hours of walking ,you reach a fork in the path ")
+def welcome():
+   print("🕵️.The Lost Treasure")
+   print("You are an explorer deep in an ancient jungle.\n"
+   "Legends say a proceless treasure lies hidden somewhere\n" 
+   "within. After hours of walking ,you reach a fork in the path ")
 
-              
-
-while True:
-    print("\nChoice 1: Go left toward the sound of running water\n"
-          "Choice 2: Go right toward a dark cave entrance.\n")
-    Choice = input("Choice: ")
-    if Choice == "1":
-       print("\nYou reach a wide river. The current looks strong.\n"
+def act1():
+      print("\nYou reach a wide river. The current looks strong.\n"
              "There's a pile of wood nearby.\n"
              "You must decide what to do")
-       print("Choice 1: Try to swim across.\n"
+      print("Choice 1: Try to swim across.\n"
              "Choice 2: Build a raft from wood.\n"
              "Choice 3: Go Back to The Fork.\n")
-       while True:
+      while True:
          Choice = input("Choice: ")
          if Choice == "1":
             print("\nThe current sweeps you away.\n"
@@ -29,15 +24,16 @@ while True:
             break
          else:
             print("Invalid Choice")
-            continue   
-    elif Choice == "2":
-        print("\nYou step into dark cave.it's cold and echoes\n"
-              "with strange noises.\n"
-              "You have a matchbox in your pocket.\n")
-        print("\nChoice 1: Light atorch.\n"
-              "Choice 2: Walk ahead in darkness.\n" 
-              "Choice 3: Go back to the fork. ")
-        while True:
+            continue
+         
+def act2():
+      print("\nYou step into dark cave.it's cold and echoes\n"
+            "with strange noises.\n"
+            "You have a matchbox in your pocket.\n")
+      print("\nChoice 1: Light atorch.\n"
+            "Choice 2: Walk ahead in darkness.\n" 
+            "Choice 3: Go back to the fork. ")
+      while True:
             Choice = input("\nChoice: ")
             if Choice == "1":
                print("You scare away a swarm of bats \n"
@@ -51,8 +47,22 @@ while True:
                break
             else:
                print("Invalid Choice.")
-               continue
-    else :
+               continue            
+
+def main():
+    welcome()
+    while True:
+      print("\nChoice 1: Go left toward the sound of running water\n"
+            "Choice 2: Go right toward a dark cave entrance.\n")
+      Choice = input("Choice: ")
+      if Choice == "1":
+           act1()
+      elif Choice == "2":
+          act2()
+
+      else :
         print("Invalid choice")
         continue
        
+if __name__=="__main__":
+    main()
